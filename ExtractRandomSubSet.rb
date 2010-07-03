9800827410439d5a872696891ff0c01b5de2494f
Julian Schrader <mail@julianschrader.com>
2010-07-03 14:44:24 +0200

    Setting up the script's sceleton

---
+++ ExtractRandomSubSet.rb
@@ -0,0 +1,20 @@
+#!/usr/local/bin/ruby -w
+
+def quit
+  puts "Extracts a random subset of CSV data"
+  puts "\nWritten by Julian Schrader\nmail@julianschrader.com -- http://julianschrader.com\n\n"
+  puts "See README for a HowTo: http://github.com/schrader/RcsvExtract"
+end
+
+def init
+  # READ ARGS
+  
+  # COUNT -- count file; quit
+  
+  # EXTRACT -- extract number of lines from file_read and save to file_destination; quit
+  
+  # No ARGS -- quit
+end
+
+init
+quit
